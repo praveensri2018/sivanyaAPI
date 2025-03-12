35d2a95c217333d43ef62d70a1a80acaa4274bf1
Praveen Bala <85008863+praveensri2018@users.noreply.github.com>
2025-03-12 16:27:27 +0530

    Update payments.js

--- routes/payments.js
+++ routes/payments.js
@@ -86,13 +86,14 @@ router.get('/user/:user_id', async (req, res) => {
 });
 
 router.get('/admin', async (req, res) => {
+    const { user_id } = req.params;
     try {
         const query = `
      SELECT p.payment_id, p.order_id, u.name AS user_name, p.amount, p.payment_method, p.payment_reference, p.payment_date
                 FROM public.Payments p JOIN public.Orders o ON p.order_id = o.order_id JOIN public.Users u ON o.user_id = u.user_id 
      ORDER BY p.payment_date DESC
         `;
-        const result = await client.query(query);
+        const result = await client.query(query,[user_id]);
 
         if (result.rows.length === 0) {
             return res.status(404).json({ message: "No payments found for this user" });
